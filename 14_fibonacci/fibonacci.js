0b2bd14d8060d65dbfa00460d6c3ab2c857b0d47
FhuSoe <rnurmarianto@yahoo.com>
2025-09-15 17:50:26 +0700

    Written the code for the function with While loop

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num == 1)
+        return 0;
+    if (num == 2)
+        return 0;
+    let num1 = 0;
+    let num2 = 1;
+    let sum;
+    let i = 2;
+    while (i < num) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+        i += 1;
+    };
+    return num2;
 };
 
 // Do not edit below this line
