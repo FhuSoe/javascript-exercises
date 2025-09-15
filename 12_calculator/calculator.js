89c99c56c47a7775cae62c7cb47229a39be75fce
FhuSoe <rnurmarianto@yahoo.com>
2025-09-15 16:06:19 +0700

    Added the parameters to the multiply function

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,17 +1,17 @@
 const add = function(a, b) {
-	return a + b
+	return a + b;
 };
 
 const subtract = function(a, b) {
-	return a - b
+	return a - b;
 };
 
 const sum = function(array) {
 	return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  return a * b;
 };
 
 const power = function() {
