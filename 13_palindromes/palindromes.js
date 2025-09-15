3198a6566e28fa615fdca73c9ca135c80eefcafc
FhuSoe <rnurmarianto@yahoo.com>
2025-09-15 17:15:07 +0700

    Written the code for the palindrome function

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const uniformString = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversedString = uniformString.split('').reverse().join('');
+    return uniformString === reversedString ;
 };
 
 // Do not edit below this line
